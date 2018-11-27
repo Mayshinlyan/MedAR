de02f7bf429aae70af17185a62b56b7057364af9
Mayshinlyan <hwai@u.rochester.edu>
2018-11-27 08:44:09 -0500

    Working Dialog System
    - Move forward and backward
    - Image and text uploads

---
+++ App/Script/CubeInteractions.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class CubeInteractions : MonoBehaviour, IFocusable, IInputClickHandler, ISpeechHandler {
+	
+	public Color NormalColor;
+	public Color HighLightColor;
+	private Renderer myRenderer;
+	private Material myMaterialInstance;
+	private bool canRotate;
+	private Vector3 initialRotation;
+	private Vector3 updatedRotation;
+
+	private void Awake(){
+		myRenderer = gameObject.GetComponent<Renderer>();
+		myMaterialInstance = myRenderer.material;
+		initialRotation = gameObject.transform.localRotation.eulerAngles;
+	}
+
+
+	private void Update(){
+		if(canRotate){
+			updatedRotation = Vector3.zero;
+			updatedRotation.x = 1; 
+			transform.localRotation *= Quaternion.Euler(updatedRotation);
+		}
+	}
+	public void OnFocusEnter(){
+		myMaterialInstance.color = HighLightColor;
+	}
+
+	public void OnFocusExit(){
+		myMaterialInstance.color = NormalColor;
+	}
+
+	private void onDestroy(){
+		Destroy (myMaterialInstance);
+	}
+
+	public void OnInputClicked(InputClickedEventData eventData){
+		Debug.Log ("Clicked");
+	}
+
+	public void OnSpeechKeywordRecognized(SpeechEventData eventData){
+
+		switch (eventData.RecognizedText.ToLower()) {
+			case "rotate":
+				startRotation();
+				break;
+		case "stop":
+				stopRotation();
+				break;
+			default:
+				break;
+		}
+	}
+
+	public void startRotation(){
+		canRotate = true;
+	}
+
+	public void stopRotation(){
+		canRotate = false;
+	}
+}
---
+++ App/Script/DialogInteractions.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+using UnityEngine.EventSystems;
+[System.Serializable]
+public class DialogInteractions : MonoBehaviour, IFocusable, IInputClickHandler, ISpeechHandler {
+
+	private bool next;
+	private bool prev;
+
+	public Dialogue dialogue;
+
+
+	public void OnInputClicked(InputClickedEventData eventData){
+		Debug.Log ("Clicked");
+	}
+		
+
+	public void OnSpeechKeywordRecognized(SpeechEventData eventData){
+
+		switch (eventData.RecognizedText.ToLower()) {
+		case "next":
+			nextDialogue();
+			break;
+		case "prev":
+			prevDialogue();
+			break;
+		default:
+			break;
+		}
+	}
+
+	public void nextDialogue(){
+		Debug.Log("Next Dialog!");
+	}
+
+	public void prevDialogue(){
+		Debug.Log("Previous Dialog!");
+	}
+
+
+	public void OnFocusEnter(){
+		
+	}
+
+	public void OnFocusExit(){
+		
+	}
+}
